fe6d4c355888d2d99e51276246176bb91a6149b5
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-05-14 08:40:11 +0000

    認証機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # 顧客用
+  get  "/signup", to: "public/registrations#new"
+  post "/signup", to: "public/registrations#create"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  scope module: :public do
+    resource :session, only: [:new, :create, :destroy]
+    resources :passwords, param: :token
+  end
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # 管理者用
+  namespace :admin do
+    resource :session, only: [:new, :create, :destroy]
+  end
 end
