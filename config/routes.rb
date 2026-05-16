53cb526afed1e8bbd8839347bd77ecc714dfa6e5
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-05-16 09:42:37 +0000

    ルーティング中

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     get "about", to: "homes#about"
+    resource :customer, only: [:show, :edit, :update]
     resource :session, only: [:new, :create, :destroy]
     resources :passwords, param: :token
   end
