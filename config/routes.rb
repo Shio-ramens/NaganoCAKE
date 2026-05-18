f19adb58df1aa8d522e50fdf0481791186c21acb
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-05-18 08:04:11 +0000

    feat: 管理者ログイン画面の作成、ルーティング設定、およびヘッダーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,10 @@ Rails.application.routes.draw do
 
   # 管理者用
   namespace :admin do
-    resource :session, only: [:new, :create, :destroy]
+    get    'sign_in',  to: 'sessions#new',     as: :new_session
+    post   'sign_in',  to: 'sessions#create',  as: :session
+    delete 'sign_out', to: 'sessions#destroy', as: :destroy_session
+
+    root to: 'homes#top'
   end
 end
