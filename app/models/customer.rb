308040d0b1024f22d8bf4b7e020756611e8dbea8
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-05-18 02:49:25 +0000

    顧客情報編集画面完成

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,17 @@ class Customer < ApplicationRecord
   has_many :sessions, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :email_address, presence: true
+
+  validates :password,
+  length: { minimum: 6 },
+  allow_nil: true
 end
