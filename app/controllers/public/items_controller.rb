36341a445f44cfe3cc7b95338db867aeb6d7bf5e
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-05-18 07:18:18 +0000

    商品一覧、詳細ページ仮作成

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,10 @@
-class Public::ItemsController < ApplicationController
+class Public::ItemsController < Public::ApplicationController
+
+  def index
+    @items = Item.where(is_active: true)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
 end
