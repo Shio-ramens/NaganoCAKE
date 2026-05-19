e20208ef336d3675ad7860036316df3a40a0c2da
Ubuntu <ubuntu@ip-172-31-41-223.ap-northeast-1.compute.internal>
2026-05-19 01:47:33 +0000

    ジャンル管理機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -49,6 +49,8 @@ Rails.application.routes.draw do
     get    'sign_in',  to: 'sessions#new',     as: :sign_in
     post   'sign_in',  to: 'sessions#create'
     delete 'sign_out', to: 'sessions#destroy', as: :sign_out
+   
+    resources :genres, only: [:index, :create, :edit, :update]
 
     root to: 'homes#top'
   end
