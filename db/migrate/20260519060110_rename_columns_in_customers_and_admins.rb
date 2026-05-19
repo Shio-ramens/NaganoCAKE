aa886b21b3fb67e0cb395ba13cc6fce010688994
Ubuntu <ubuntu@ip-172-31-31-236.ap-northeast-1.compute.internal>
2026-05-19 07:12:06 +0000

    マイグレーションファイル再変更

--- db/migrate/20260519060110_rename_columns_in_customers_and_admins.rb
+++
@@ -1,9 +0,0 @@
-class RenameColumnsInCustomersAndAdmins < ActiveRecord::Migration[8.0]
-def change
-    rename_column :customers, :email, :email
-    rename_column :customers, :encrypted_password, :encrypted_password
-
-    rename_column :admins, :email, :email
-    rename_column :admins, :encrypted_password, :encrypted_password
-  end
-end
